ef7c3baaad3d3fbf9c025bc023cde475c5f09482
christian joel <nanakepbawou@gmail.com>
2026-04-13 22:50:43 +0100

    Fix: correct static routing for Vercel assets

--- Phone_Book/Back_end_phone_book/Back_end_phone_book/index.js
+++ Phone_Book/Back_end_phone_book/Back_end_phone_book/index.js
@@ -9,7 +9,8 @@ const path = require('path');
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.json());
-app.use(express.static('dist'));
+
+//app.use(express.static('dist'));
 
 //app.use(morgan('tiny'))
 
